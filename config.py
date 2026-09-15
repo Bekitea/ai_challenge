7eadbd8d380522ec83130ac3b24178e603e25d3d
Zakharov_Rostislav <r.z.8102.80@gmail.com>
2026-09-15 20:34:46 +0400

    chore: поправил конфиг для БД

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ YANDEX_API_KEY = os.getenv("YANDEX_CLOUD_API_KEY")
 YANDEX_FOLDER_ID = os.getenv("YANDEX_CLOUD_FOLDER")
 YANDEX_DEFAULT_MODEL = "aliceai-llm-flash/latest"
 
-DATABASE_PATH = os.getenv("DATABASE_PATH", "./data/agents.db")
+DATABASE_PATH = "./data/agents.db" # если это нужно поменять, то и в настройках alimbic тоже
 DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
-CHAT_HISTORY_DIR = os.getenv("CHAT_HISTORY_DIR", "./data/chat_history")
+CHAT_HISTORY_DIR = "./data/chat_history"
 
 Path(DATABASE_PATH).parent.mkdir(parents=True, exist_ok=True)
 Path(CHAT_HISTORY_DIR).mkdir(parents=True, exist_ok=True)
